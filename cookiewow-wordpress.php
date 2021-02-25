f4ce9e7e6dea3e691490e3daf6ac07d38a738a59
Saroj Sangphongamphai <ss.saroj@gmail.com>
2021-02-25 09:34:14 +0700

    feat: sanitize settings fields

--- cookiewow-wordpress.php
+++ cookiewow-wordpress.php
@@ -10,20 +10,12 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-function cookiewow_admin_menu() {
-	add_menu_page(
-		$page_title = 'Cookie Wow Settings',
-		$menu_title = 'Cookie Wow',
-		$capability = 'manage_options',
-		$menu_slug  = 'cookiewow-settings',
-		$function   = 'cookiewow_settings_page',
-		$icon_url   = 'dashicons-admin-plugins',
-		$position   = '25'
-	);
-}
-
 function cookiewow_admin_init() {
-	register_setting( 'cookiewow_settings_fields', 'cookiewow_option' );
+	register_setting(
+		$option_group = 'cookiewow_settings_fields',
+		$option_name  = 'cookiewow_option',
+		$args         = array( 'sanitize_callback' => 'cookiewow_sanitize_settings_fields' )
+	);
 
 	add_settings_section(
 		$id       = 'setting_section_id',
@@ -42,11 +34,23 @@ function cookiewow_admin_init() {
 	);
 }
 
+function cookiewow_admin_menu() {
+	add_menu_page(
+		$page_title = 'Cookie Wow Settings',
+		$menu_title = 'Cookie Wow',
+		$capability = 'manage_options',
+		$menu_slug  = 'cookiewow-settings',
+		$function   = 'cookiewow_settings_page',
+		$icon_url   = 'dashicons-admin-plugins',
+		$position   = '25'
+	);
+}
+
 function cookiewow_settings_fields() {
-	$options = get_option( 'cookiewow_option' );
+	$option = get_option( 'cookiewow_option' );
 	printf(
 		'<input type="text" id="token" name="cookiewow_option[token]" class="regular-text" value="%s" />',
-		isset( $options['token'] ) ? esc_attr( $options['token']) : ''
+		isset( $option['token'] ) ? esc_attr( $option['token']) : ''
 	);
 }
 
@@ -54,8 +58,18 @@ function cookiewow_settings_page() {
 	require_once plugin_dir_path( __FILE__ ) . 'settings.php';
 }
 
+function cookiewow_sanitize_settings_fields( $fields ) {
+	$sanitized_fields = array();
+
+	if ( isset( $fields['token'] ) ) {
+		$sanitized_fields['token'] = sanitize_text_field( ( $fields['token'] ) );
+	}
+
+	return $sanitized_fields;
+}
+
 function cookiewow_settings_section_description() {
 }
 
-add_action( 'admin_menu', 'cookiewow_admin_menu' );
 add_action( 'admin_init', 'cookiewow_admin_init');
+add_action( 'admin_menu', 'cookiewow_admin_menu' );
