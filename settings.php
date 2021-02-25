4d9cd4c247541873e55d2bac4272c9be6f569ce4
Saroj Sangphongamphai <ss.saroj@gmail.com>
2021-02-25 10:00:07 +0700

    style: correct the indentation

--- settings.php
+++ settings.php
@@ -2,9 +2,9 @@
 	<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
 	<form action="options.php" method="post">
 		<?php
-            settings_fields( 'cookiewow_settings_fields' );
-            do_settings_sections( 'cookiewow-settings' );
-            submit_button();
+		settings_fields( 'cookiewow_settings_fields' );
+		do_settings_sections( 'cookiewow-settings' );
+		submit_button();
 		?>
 	</form>
 </div>
