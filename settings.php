502edd5da27019eaebb17f6548700c3f8a6f130d
Saran Angsuwan <saran.a@datawow.io>
2021-03-01 18:45:23 +0700

    feat: added installation instruction to setting page

--- settings.php
+++ settings.php
@@ -16,13 +16,74 @@
 		submit_button();
 		?>
 	</form>
-    <!-- <hr />
-    <section class="cookiewow-howto">
-        <h1>How to...</h1>
-        <ol>
-            <li>Coffee</li>
-            <li>Tea</li>
-            <li>Milk</li>
-        </ol>
-    </section> -->
+    <hr />
+    <section class="cookiewow-howto-container">
+        <article class="cookiewow-howto">
+            <h3>
+                Sign in to
+                <a
+                    href="https://cookiewow.com/"
+                    target="_blank"
+                    rel="norefferer"
+                >
+                    Cookie Wow
+                </a>
+                (Or sign up
+                    <a
+                        href="https://app.cookiewow.com/users/sign_up"
+                        target="_blank"
+                        rel="norefferer"
+                    >here</a>
+                )
+            </h3>
+            <ol>
+                <li>Add your wordpress domain</li>
+                <li>Go to your domain dashboard and click Customize</li>
+                <li>Copy Banner ID from “Get script” button</li>
+                <li>Paste Banner ID on Wordpress plug-in and click “Save Changes”</li>
+            </ol>
+            <p>Read full instruction
+                <a
+                    href="https://cookiewow.crisp.help/th/article/cookie-wow-wordpress-16sumbv/"
+                    target="_blank"
+                    rel="norefferer"
+                >
+                    here
+                </a>
+            </p>
+            <hr />
+        </article>
+        <article class="cookiewow-howto">
+            <h3>
+                เข้าสู่ระบบ
+                <a
+                    href="https://cookiewow.com/"
+                    target="_blank"
+                    rel="norefferer"
+                >Cookie Wow</a>
+                (หรือลงชื่อเข้าใช้
+                    <a
+                        href="https://app.cookiewow.com/users/sign_up"
+                        target="_blank"
+                        rel="norefferer"
+                    >ที่นี่</a>
+                )
+            </h3>
+            <ol>
+                <li>เพิ่มโดเมนของเว็บไซต์ WordPress ของคุณ</li>
+                <li>ไปที่แดชบอร์ดของ Cookie Wow แล้วคลิกที่เมนู Customize</li>
+                <li>คลิกที่ปุ่ม “Get Script” แล้วคัดลอก Banner ID</li>
+                <li>วาง Banner ID บน Plugin ของ WordPress แล้วคลิก “Save Changes”</li>
+            </ol>
+            <p>อ่านวิธีการติดตั้งฉบับเต็ม
+                <a
+                    href="https://cookiewow.crisp.help/th/article/cookie-wow-wordpress-16sumbv/"
+                    target="_blank"
+                    rel="norefferer"
+                >
+                    ที่นี่
+                </a>
+            </p>
+        </article>
+    </section>
 </div>
