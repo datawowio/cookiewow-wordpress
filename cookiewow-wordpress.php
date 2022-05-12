6fda0632d3d4b1cdb38c6f3ef005f6b1b5b6e700
Sugata Koufuku <sugakou44@gmail.com>
2022-05-12 14:55:05 +0700

    feat: added data-cfasync attribute to make rocket-loader ignore the scripts

--- cookiewow-wordpress.php
+++ cookiewow-wordpress.php
@@ -174,7 +174,16 @@ function cookiewow_script_loader_tag( $tag, $script_name, $src ) {
 		$replace_to_position = -1;
 		return substr_replace(
 			$tag,
-			"id='cookieWow' type='text/javascript' src='$src' data-cwcid='$token'></script>",
+			"id='cookieWow' type='text/javascript' src='$src' data-cwcid='$token' data-cfasync='false'></script>",
+			$replace_at_position,
+			$replace_to_position
+		);
+	} elseif ( 'cookiewow_script' === $script_name ) {
+		$replace_at_position = strpos( $tag, 'src' );
+		$replace_to_position = -1;
+		return substr_replace(
+			$tag,
+			"src='$src' data-cfasync='false'></script>",
 			$replace_at_position,
 			$replace_to_position
 		);
